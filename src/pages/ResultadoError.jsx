3fb309932a6159470b605252dd6473cf81dab64c
JC <ju.carmonag@duocuc.cl>
2025-11-08 11:57:52 -0300

    cambios en proyecto debido a errores

---
+++ src/pages/ResultadoError.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function ResultadoError(){
+  return <div className="text-center"><h2>Ocurrió un problema con tu compra</h2></div>
+}
---
+++ src/pages/ResultadoOk.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function ResultadoOk(){
+  return <div className="text-center"><h2>Compra realizada con éxito</h2></div>
+}
